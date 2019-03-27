be6dab46f2f93a3dd7e5dbb36d66ad809e0757cc
StolarQQ <stolar28@gmail.com>
2019-03-27 17:57:27 +0100

    added measurment model

---
+++ src/WebApiServer/Model/Measurment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiServer.Model
+{
+    public class Measurment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(4,2)"]
+        public decimal Value { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        
+        public Measurment(int id, string name, decimal value, string createdBy, DateTime updatedAt)
+        {
+            Id = id;
+            Name = name;
+            Value = value;
+            CreatedBy = createdBy;
+            CreatedAt = updatedAt;
+        }
+
+
+
+    }
+}
