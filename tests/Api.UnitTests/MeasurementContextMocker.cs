7251a8892a905192244076c7706fd7b151674272
StolarQQ <stolar28@gmail.com>
2019-04-10 19:45:03 +0100

    Added mock and extenstion class for MeasurementContext

---
+++ tests/Api.UnitTests/MeasurementContextMocker.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiServer.Model;
+using WebApiServer.DbContexts;
+using WebApiServer.Repository;
+
+namespace Api.UnitTests
+{
+    public static class MeasurementContextMocker
+    {
+        public static IMeasurementRepository<Measurement> GetInMemoryMeasurementsRepository(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<MeasurementContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            MeasurementContext measurmentContext = new MeasurementContext(options);
+            measurmentContext.FillDatabase();
+
+            return new MeasurmentRepository(measurmentContext);
+        }
+    }
+}
