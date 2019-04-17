7f2d52e6dc7b69b3466753895941ce9f404b8655
StolarQQ <stolar28@gmail.com>
2019-04-17 16:54:36 +0100

    Added newMeasurmentSection.js

---
+++ src/WebClient/wwwroot/js/newMeasurmentSection.js
@@ -0,0 +1,28 @@
+﻿class NewMeasurmentSection {
+
+    _listeners = []
+
+    constructor() {
+        let addBtn = document.querySelector('#measurment_addBtn')
+        addBtn.addEventListener('click', x => {
+            let nameInput = document.querySelector('#measurment_nameInput')
+            let valueInput = document.querySelector('#measurment_valueInput')
+            let obj = {
+                name: nameInput.value,
+                value: valueInput.value
+            }
+            this._raiseNewMeasurmentAdded(obj)            
+        })
+
+    }
+
+    addEventListener(listener) {
+        this._listeners.push(listener)
+    }
+
+    _raiseNewMeasurmentAdded(e) {
+        this._listeners.forEach(l => {
+            l.NewMeasurmentAdded(e)
+        })
+    }
+}
