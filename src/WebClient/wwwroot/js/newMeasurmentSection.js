3ad6de668a9894e74df4d230b53c0cbba9dea694
StolarQQ <stolar28@gmail.com>
2019-04-17 18:18:46 +0100

    Added missing semicolons

--- src/WebClient/wwwroot/js/newMeasurmentSection.js
+++ src/WebClient/wwwroot/js/newMeasurmentSection.js
@@ -3,21 +3,20 @@
     _listeners = []
 
     constructor() {
-        let addBtn = document.querySelector('#measurment_addBtn')
+        let addBtn = document.querySelector('#measurment_addBtn');
         addBtn.addEventListener('click', x => {
-            let nameInput = document.querySelector('#measurment_nameInput')
-            let valueInput = document.querySelector('#measurment_valueInput')
+            let nameInput = document.querySelector('#measurment_nameInput');
+            let valueInput = document.querySelector('#measurment_valueInput');
             let obj = {
                 name: nameInput.value,
                 value: valueInput.value
-            }
-            this._raiseNewMeasurmentAdded(obj)            
-        })
-
+            };
+            this._raiseNewMeasurmentAdded(obj);
+        });
     }
 
     addEventListener(listener) {
-        this._listeners.push(listener)
+        this._listeners.push(listener);
     }
 
     _raiseNewMeasurmentAdded(e) {
