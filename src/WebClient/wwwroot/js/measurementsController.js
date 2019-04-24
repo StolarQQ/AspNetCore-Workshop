0fda2e7a3834d22f301dd17890eaa1e9df9de161
StolarQQ <stolar28@gmail.com>
2019-04-24 18:14:26 +0100

    Added Controller for measurement site

---
+++ src/WebClient/wwwroot/js/measurementsController.js
@@ -0,0 +1,22 @@
+﻿
+class MeasurmentController {
+
+    constructor() {
+        this._newMeasurementSection = new NewMeasurmentSection();
+        this._newMeasurementListSection = new MeasurementListSection();
+        let _this = this;
+
+
+        this._newMeasurementSection.addEventListener(new class {
+            NewMeasurementAdded(e) {
+                _this._newMeasurementSection.addEventListener(e);
+            }
+        });
+
+        }
+
+}
+
+
+
+(() => new MeasurmentController())();   
